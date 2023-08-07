dbe173e10d76b33f188e3729626806609e066713
Daniel Olivet <olivetjimenezdaniel@gmail.com>
2023-08-07 20:53:16 +0200

    feat: LoginUserValidator object created & LoginUserController added

---
+++ backend/src/User/Application/Validate/LoginUserValidator.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\User\Application\Validate;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+final class LoginUserValidator
+{
+    #[Assert\NotBlank()]
+    public string $email;
+
+    #[Assert\Email(
+        message:'The email {{ value }} is not a valid email.',
+    )]
+    #[Assert\NotBlank]
+    public string $password;
+
+    public function __construct( string $email, string $password )
+    {
+        $this->email    = $email;
+        $this->password = $password;
+    }
+}
---
+++ backend/src/User/Infrastructure/Controller/LoginUserController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\User\Infrastructure\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class LoginUserController extends AbstractController
+{
+
+}
